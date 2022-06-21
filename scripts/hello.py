161452ffab822acfc008e6e6ebcb67619f316e42
Lawrence Mitchell <lmitchell@nvidia.com>
2022-06-21 08:20:33 -0700

    Minor updates to job script

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,9 +3,10 @@ import sys
 from pprint import pprint
 
 import cupy as xp
+import time
+import sortedcontainers.sorteddict
 from dask import array as da
 from dask.distributed import Client, wait
-
 
 def get_workers(dask_scheduler=None):
     return dask_scheduler.workers
@@ -13,26 +14,23 @@ def get_workers(dask_scheduler=None):
 
 def run():
     with Client(scheduler_file=os.path.abspath(sys.argv[1])) as client:
-        while len(client.scheduler_info()["workers"]) < int(
+        while len(client.scheduler_info().get("workers", [])) < int(
             os.environ["EXPECTED_NUM_WORKERS"]
         ):
             print(
                 "Waiting for workers to come up, "
-                f"have {len(client.scheduler_info()['workers'])}, "
+                f"have {len(client.scheduler_info().get('workers', []))}, "
                 f"want {os.environ['EXPECTED_NUM_WORKERS']}"
             )
-        #     time.sleep(5)
-        rs = da.random.RandomState(RandomState=xp.random.RandomState)
-        x = rs.random((2500, 2500), chunks=1000).persist()
-        wait(x)
-        fn = lambda x: (x + x.T).sum()
-        print("start")
-        wait(client.persist(fn(x)))
-        print("stop")
-        workers = client.run_on_scheduler(get_workers)
-        for worker in workers.values():
-            pprint(worker)
-            pprint(worker.name)
+            time.sleep(5)
+        try:
+            workers = client.run_on_scheduler(get_workers)
+            for worker in workers.values():
+                pprint(worker)
+                pprint(worker.name)
+        except Exception as e:
+            print("run on scheduler failed, but meh.")
+            print(e)
         print("Shutting down scheduler/workers")
         client.shutdown()
         client.close()
--- scripts/job.sh
+++ scripts/job.sh
@@ -8,23 +8,31 @@ SCHED_FILE=${SCRATCHDIR}/scheduler-${SLURM_JOBID}.json
 NGPUS=$(nvidia-smi -L | wc -l)
 export EXPECTED_NUM_WORKERS=$((SLURM_JOB_NUM_NODES * NGPUS))
 
+export WORKER_ARGS="--protocol ucx --enable-nvlink --enable-infiniband"
+
 # Idea: we allocate ntasks-per-node for workers, but those are started in the
 # background by dask-cuda-worker.
 # So we need to pick one process per node to run the worker commands.
 # This assumes that the mapping from nodes to ranks is dense and contiguous. If
 # there is rank-remapping then something more complicated would be needed.
-if [[ $(((SLURM_PROCID / SLURM_NTASKS_PER_NODE) * SLURM_PROCID)) == $SLURM_PROCID ]]; then
+
+if [[ $(((SLURM_PROCID / SLURM_NTASKS_PER_NODE) * SLURM_NTASKS_PER_NODE)) == ${SLURM_PROCID} ]]; then
     # rank zero starts scheduler and client as well
     if [[ $SLURM_NODEID == 0 ]]; then
         echo "${SLURM_PROCID} on node ${SLURM_NODEID} starting scheduler/client"
-        dask-scheduler --scheduler-file ${SCHED_FILE} &
+        dask-scheduler --no-dashboard --protocol ucx --scheduler-file ${SCHED_FILE} &
         sleep 6
-        dask-cuda-worker --scheduler-file ${SCHED_FILE} &
-        python ${RUNDIR}/hello.py ${SCHED_FILE}
+        dask-cuda-worker --no-dashboard ${WORKER_ARGS} --scheduler-file ${SCHED_FILE} &
+        python ${RUNDIR}/local_cupy.py -s 50000 -c 2500 \
+            --runs 10 \
+            --benchmark-json ${OUTDIR}/run.json \
+            --scheduler-file ${SCHED_FILE} \
+            ${WORKER_ARGS} # ignored, but for correct data in output files
+        # python ${RUNDIR}/hello.py ${SCHED_FILE}
     else
         echo "${SLURM_PROCID} on node ${SLURM_NODEID} starting worker"
         sleep 6
-        dask-cuda-worker --scheduler-file ${SCHED_FILE}
+        dask-cuda-worker --no-dashboard ${WORKER_ARGS} --scheduler-file ${SCHED_FILE}
     fi
 else
     echo "${SLURM_PROCID} on node ${SLURM_NODEID} sitting in background"
