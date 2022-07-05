66ae42aefd33be8e53bda794901c8742638be9e2
Lawrence Mitchell <lmitchell@nvidia.com>
2022-07-05 08:03:03 -0700

    Transpose benchmark

---
+++ perlmutter-transpose/job.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+SCHED_FILE=${SCRATCHDIR}/scheduler-${SLURM_JOBID}.json
+
+# Could ask, but easier to hard-code
+# NGPUS=4
+NGPUS=4
+export EXPECTED_NUM_WORKERS=$((SLURM_JOB_NUM_NODES * NGPUS))
+
+export COMMON_ARGS="--protocol ${PROTOCOL} \
+       --interface hsn0 \
+       --scheduler-file ${SCRATCHDIR}/scheduler-${SLURM_JOBID}.json"
+export PROTOCOL_ARGS=""
+# export WORKER_ARGS="--shared-filesystem \
+#        --local-directory ${SCRATCHDIR}/tmp-${SLURM_JOBID}-${SLURM_PROCID} \
+#        --multiprocessing-method forkserver"
+export WORKER_ARGS="--local-directory /dev/shm --multiprocessing-method forkserver"
+# Turn off ptxcompiler subprocess call
+export PTXCOMPILER_CHECK_NUMBA_CODEGEN_PATCH_NEEDED=0
+export UCX_SOCKADDR_TLS_PRIORITY=rdmacm
+# Warn if fork after init
+# export UCX_IB_FORK_INIT=n
+
+# Submit with --gpus-per-node NGPU --ntasks-per-node 1 --cpus-per-task NGPU (or 2xNGPU)
+
+# Idea: we allocate ntasks-per-node for workers, but those are started in the
+# background by dask-cuda-worker.
+# So we need to pick one process per node to run the worker commands.
+# This assumes that the mapping from nodes to ranks is dense and contiguous. If
+# there is rank-remapping then something more complicated would be needed.
+if [[ $(((SLURM_PROCID / SLURM_NTASKS_PER_NODE) * SLURM_NTASKS_PER_NODE)) == ${SLURM_PROCID} ]]; then
+    # rank zero starts scheduler and client as well
+    if [[ $SLURM_NODEID == 0 ]]; then
+        echo "${SLURM_PROCID} on node ${SLURM_NODEID} starting scheduler/client"
+        python -m distributed.cli.dask_scheduler \
+               --no-dashboard \
+               ${COMMON_ARGS} &
+        sleep 6
+        python -m dask_cuda.cli.dask_cuda_worker \
+               --no-dashboard \
+               ${COMMON_ARGS} \
+               ${PROTOCOL_ARGS} \
+               ${WORKER_ARGS} &
+        # TODO: Better json data naming
+        # TODO: scaling parameters?
+        # TODO: Parameterize sizes
+        # TODO: What to run in a single allocation?
+        echo "${SLURM_PROCID} on node ${SLURM_NODEID} starting worker"
+        python ${RUNDIR}/local_cupy.py \
+               -s ${DATA_SIZE} \
+               --runs 10 \
+               ${COMMON_ARGS} \
+               ${PROTOCOL_ARGS} \
+	       --shutdown-external-cluster-on-exit \
+	       --output-basename ${OUTDIR}/benchmark-data \
+               --multiprocessing-method forkserver \
+            || /bin/true        # always exit cleanly
+    else
+        echo "${SLURM_PROCID} on node ${SLURM_NODEID} starting worker"
+        sleep 6
+        python -m dask_cuda.cli.dask_cuda_worker \
+               --no-dashboard \
+               ${COMMON_ARGS} \
+               ${PROTOCOL_ARGS} \
+               ${WORKER_ARGS} \
+            || /bin/true        # always exit cleanly
+    fi
+else
+    echo "${SLURM_PROCID} on node ${SLURM_NODEID} sitting in background"
+fi
---
+++ perlmutter-transpose/submit.sh
@@ -0,0 +1,49 @@
+# Per https://docs.nersc.gov/jobs/best-practices/#improve-efficiency-by-preparing-user-environment-before-running
+# set up environment on login node
+
+module load PrgEnv-nvidia
+module load cudatoolkit
+module load gcc
+
+export PROTOCOL=ucx
+export SCRATCH=/pscratch/sd/w/wence
+export SCRATCHDIR=${SCRATCH}/dask-cuda-benchmark/scratch
+export RUNDIR=/global/common/software/dasrepo/wence/src/dask-cuda-benchmarks/perlmutter-transpose/
+mkdir -p ${SCRATCHDIR}
+
+source /global/common/software/dasrepo/wence/mambaforge/etc/profile.d/conda.sh
+source /global/common/software/dasrepo/wence/mambaforge/etc/profile.d/mamba.sh
+mamba activate dask-cuda
+
+echo "PATH=${PATH}"
+echo "PROTOCOL=${PROTOCOL}"
+echo "SCRATCHDIR=${SCRATCHDIR}"
+export DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT=3600s
+export DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP=3600s
+# export NUM_NODES=1
+# export DATA_SIZE=50000
+
+export NUM_NODES=2
+export DATA_SIZE=70000
+
+export NUM_NODES=4
+export DATA_SIZE=100000
+
+export NUM_NODES=8
+export DATA_SIZE=140000
+
+export NUM_NODES=16
+export DATA_SIZE=200000
+
+export NUM_NODES=32
+export DATA_SIZE=280000
+
+export NUM_NODES=64
+export DATA_SIZE=400000
+echo $DATA_SIZE
+export JOB_NAME=transpose
+sbatch --job-name ${JOB_NAME} \
+       --nodes ${NUM_NODES} \
+       -e slurm-%x-%j.err \
+       -o slurm-%x-%j.out \
+       ./job.slurm
